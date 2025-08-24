e8c32eb2d3814c64ba07421c0b0770cb3c2938cc
Artem Netsvetaev <physphile@vk.com>
2025-08-24 18:13:23 +0300

    убрал лишний код

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,10 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./database/db";
-import * as schema from "./database/schemas";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
-    schema,
   }),
   emailAndPassword: {
     enabled: true,
