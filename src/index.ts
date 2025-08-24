a2ee5d2b235bd29f7148173ea47908eae973fe40
Artem Netsvetaev <physphile@vk.com>
2025-08-24 18:07:31 +0300

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,50 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
+import { swagger } from "@elysiajs/swagger";
+import { auth } from "./auth";
+import { OpenAPI } from "./OpenAPI";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const betterAuth = new Elysia({ name: "better-auth" })
+  .mount("/auth", auth.handler)
+  .macro({
+    auth: {
+      async resolve({ status, request: { headers } }) {
+        const session = await auth.api.getSession({
+          headers,
+        });
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+        if (!session) return status(401);
+
+        return {
+          user: session.user,
+          session: session.session,
+        };
+      },
+    },
+  });
+
+new Elysia()
+  .use(
+    cors({
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    })
+  )
+  .use(
+    swagger({
+      path: '/docs',
+      documentation: {
+        components: await OpenAPI.components,
+        paths: await OpenAPI.getPaths(),
+      },
+    })
+  )
+  .use(betterAuth)
+  .get("/user", ({ user }) => user, {
+    auth: true,
+  })
+  .listen(3000);
+
+console.log("Swagger docs available at http://localhost:3000/docs");
